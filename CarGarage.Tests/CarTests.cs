b7da420bbb20527c54ac5b7e510dd1efdd76262e
Adam Pryatel <adampryatel@gmail.com>
2021-01-31 17:25:53 -0500

    Project Complete

--- CarGarage.Tests/CarTests.cs
+++ CarGarage.Tests/CarTests.cs
@@ -13,75 +13,75 @@ namespace CarGarage.Tests
         }
 
         [Fact]
-        public void SetMake_Assigns_Make_of_Car()
+        public void SetMake_Assigns_Make_of_Car()       // name of the method                                           // Done
         {
             car.Make = "";
 
             // Add a SetMake method to the Car class that assigns the Make property to the value passed into the method
             // Uncomment the line below:
-            // car.SetMake("Honda");
+            car.SetMake("Honda");               // Needs a method, that is why the red is here
 
             Assert.Equal("Honda", car.Make);
         }
 
         [Fact]
-        public void SetModel_Assigns_Model_of_Car()
+        public void SetModel_Assigns_Model_of_Car()                                                                     // Done
         {
             car.Model = "";
 
             // Add a SetModel method to the Car class that assigns the Model property to the value passed into the method
             // Uncomment the line below:
-            // car.SetModel("Accord");
+            car.SetModel("Accord");
 
             Assert.Equal("Accord", car.Model);
         }
 
         [Fact]
-        public void GetSpeed_Should_Return_Speed()
+        public void GetSpeed_Should_Return_Speed()                                                                     // Done
         {
             car.Speed = 27;
 
             // Add a GetSpeed method to the Car class that returns the value of the Speed property
             // Uncomment the line below:
-            // Assert.Equal(27, car.GetSpeed());
+             Assert.Equal(27, car.GetSpeed());
         }
 
         [Fact]
-        public void IsSpeeding_Should_Return_True_If_Speed_Over_70mph()
+        public void IsSpeeding_Should_Return_True_If_Speed_Over_70mph()                                                // Done
         {
             car.Speed = 71;
 
             // Add an IsSpeeding method to the Car class that returns true if the Speed property is greater than 70
             // Uncomment the line below:
-            // Assert.True(car.IsSpeeding());
+            Assert.True(car.IsSpeeding());
         }
 
         [Fact]
-        public void Accelerate_Should_Increase_Speed_By_10mph()
+        public void Accelerate_Should_Increase_Speed_By_10mph()                                                         // Done
         {
             car.Speed = 0;
 
             // Add an Accelerate method to the Car class that increases Speed by 10 mph
             // Uncomment the line below:
-            // car.Accelerate();
+            car.Accelerate();
 
             Assert.Equal(10, car.Speed);
         }
 
         [Fact]
-        public void Accelerate_Should_Not_Increase_Speed_Over_100mph()
+        public void Accelerate_Should_Not_Increase_Speed_Over_100mph()                                                    // Done
         {
             car.Speed = 100;
 
             // Modify the Accelerate method so that the car's Speed can never exceed 100 mph
             // Uncomment the line below:
-            // car.Accelerate();
+            car.Accelerate();
 
             Assert.Equal(100, car.Speed);
         }
 
         [Fact]
-        public void Accelerate_Should_Increase_Speed_By_20mph_If_Ferrari()
+        public void Accelerate_Should_Increase_Speed_By_20mph_If_Ferrari()                                                 // Done
         {
             car.Speed = 0;
             car.Make = "Ferrari";
@@ -89,19 +89,19 @@ namespace CarGarage.Tests
             // Modify the Accelerate method so that the car's Speed increases by 20mph, but only if the
             // Make is "Ferrari"
             // Uncomment the line below:
-            // car.Accelerate();
+            car.Accelerate();
 
             Assert.Equal(20, car.Speed);
         }
 
         [Fact]
-        public void Brake_Should_Decrease_Speed_by_7mph()
+        public void Brake_Should_Decrease_Speed_by_7mph()                                                                   // Done
         {
             car.Speed = 10;
 
             // Add a Brake method to the Car class that decreases Speed by 7 mph
             // Uncomment the line below:
-            // car.Brake();
+            car.Brake();
 
             Assert.Equal(3, car.Speed);
         }
@@ -113,7 +113,7 @@ namespace CarGarage.Tests
 
             // Modify the Brake method so that the car's Speed can never go below 0 mph
             // Uncomment the line below:
-            // car.Brake();
+            car.Brake();
 
             Assert.Equal(0, car.Speed);
         }
@@ -126,7 +126,7 @@ namespace CarGarage.Tests
 
             // Modify the Brake method so that the car's Speed is decreased by half if AutoBrakeActive property is true
             // Uncomment the line below:
-            // car.Brake();
+            car.Brake();
 
             Assert.Equal(25, car.Speed);
         }
--- CarGarage/Car.cs
+++ CarGarage/Car.cs
@@ -12,5 +12,77 @@ namespace CarGarage
         public int FuelLevel { get; set; }
         public bool AutoBrakeActive { get; set; }
 
+        // Write methods to pass the tests
+
+
+        public string SetMake(string make)                               // Done
+        {
+            Make = make;
+            return Make;
+        }
+
+        public string SetModel(string model)                              // Done
+        {
+            Model = model;
+            return Model;
+        }
+
+        public int GetSpeed()                                              // Done
+        {
+            return Speed;
+        }
+
+        public bool IsSpeeding()                                           // Done
+        {
+            if (Speed > 70)
+
+                return true;
+
+            else
+                return false;
+        }
+
+        public void Accelerate()                                           // Done
+        {
+
+                if (Speed < 100)
+                {
+                    if (Make == "Ferrari")
+
+                        Speed += 20;
+
+                    else
+                    {
+                        Speed += 10;
+                    }
+                }
+                else
+                {
+                    Speed = Speed;
+                }
+        }
+
+        public void Brake()
+        {
+
+            if (Speed > 7)      
+            {                                                     
+                if (AutoBrakeActive == true)         
+                {                                      
+                    Speed /= 2;
+                }
+
+
+                else 
+                {
+                    Speed -= 7;
+                }
+
+            }
+            else
+            {
+                Speed -= Speed;
+            }
+        }
     }
 }
